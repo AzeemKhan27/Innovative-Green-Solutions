81d2cbcd46679464f1a129ff0570b29bfca707fe
AzeemKhan27git config --global user.name AzeemKhan27git config --global user.name AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-11-07 22:25:43 +0530

    frontend: Index file

---
+++ innovative-frontend/app/pages/index.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold">Welcome to My Next.js App</h1>
+        <p className="mt-4 text-lg">This is a simple Next.js application.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
